7a8bfefe987688ada36da4d1e35a50c4f420af15
msulaev <msulaev@MBP-msulaev.spbfiller>
2019-07-10 11:24:18 +0300

    fix comment

--- src/test/java/OpenW9FormTest.java
+++ src/test/java/OpenW9FormTest.java
@@ -4,6 +4,7 @@ import step.Top100FormStep;
 
 import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.*;
 import static matchers.CssAttributeMatcher.cssAttribute;
 import static matchers.ExistsMatcher.isExists;
 import static org.awaitility.Awaitility.await;
@@ -24,10 +25,9 @@ class OpenW9FormTest extends BaseTest {
         onSite().open(TOP100_URL);
         top100FormStep = new Top100FormStep(driver, atlas).openW9FormFromTop100();
         await("Wait help modal")
-                .atMost(20, TimeUnit.SECONDS)
+                .atMost(20, SECONDS)
                 .ignoreExceptions()
                 .until(top100FormStep.helpModal()::getText, equalTo("Need Help?"));
-        assertThat(top100FormStep.helpModal(), cssAttribute("color", equalTo("")));
         assertThat(top100FormStep.helpModal(), isExists());
     }
 }
